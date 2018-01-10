0435c33b98fb3869f4054e8f1545f2311a958cca
Leonardo Chaves Machado <lmachado@tecgraf.puc-rio.br>
2018-01-10 13:16:04 -0200

    Script jython para ser colocado no Gluu - projeto eclipse

---
+++ java/GluuScript/src/CSBaseTest.py
@@ -0,0 +1,29 @@
+'''
+Created on Jan 10, 2018
+
+@author: lcm
+'''
+from csbase.util.proxy import CSBaseLoginProxy
+
+import java
+import csbase
+
+class CSBaseTest(object):
+    '''
+    classdocs
+    '''
+    csbaseProxy = csbase.util.proxy.CSBaseLoginProxy
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        print "CSBase. Initialization"
+        self.csbaseProxy = CSBaseLoginProxy("localhost","5099", True)
+        #print "CSBase. Initialized successfully"
+    def authenticate(self):
+        logged_in = self.csbaseProxy.authenticate("lmachado", "leo", java.util.Locale.US)
+        print logged_in
+        logged_in = self.csbaseProxy.authenticate("admin", "1234555", java.util.Locale.US)
+        print logged_in
---
+++ java/GluuScript/src/main.py
@@ -0,0 +1,10 @@
+'''
+Created on Jan 8, 2018
+
+@author: lcm
+'''
+from CSBaseTest import CSBaseTest
+
+if __name__ == "__main__":
+    csbaseTest = CSBaseTest()
+    csbaseTest.authenticate()

068ce9942f60bf0162f3b7d8c52df5a810e667f4
Leonardo Chaves Machado <lmachado@tecgraf.puc-rio.br>
2018-01-26 07:59:03 -0200

    script com metodos separados para acessar csbase e criar usuario

--- java/GluuScript/src/CSBaseRestAuthenticator.py
+++ java/GluuScript/src/CSBaseRestAuthenticator.py
@@ -50,49 +50,18 @@ class PersonAuthentication(PersonAuthenticationType):
             authenticationService = CdiUtil.bean(AuthenticationService)
             userService = CdiUtil.bean(UserService)
             
-            print "CSBaseRest: username: "+user_name+" - passwd: "+user_password
             logged_in = False
             if (StringHelper.isNotEmptyString(user_name) and StringHelper.isNotEmptyString(user_password)):
-                #responseData = self.getRestDataFor(configurationAttributes.get("CSBaseRestBaseURL").getValue2(), user_name, user_password)
                 # Acessa o CSBASE para saber se login e senha estao ok
-                baseurl = configurationAttributes.get("CSBaseRestBaseURL").getValue2()
-                print "CSBaseRest: acessando csbase na url base: "+baseurl
-                params = urllib.urlencode({'login': user_name, 'password': user_password})
-                headers = {"Content-type": "application/x-www-form-urlencoded"}
-                print "CSBaseRest: conectando..."
-                conn = httplib.HTTPConnection(baseurl)
-                print "CSBaseRest: fazendo requisicao..."
-                conn.request("POST", "/v1/authentication", params, headers)
-                print "CSBaseRest: obtendo reposta..."
-                response = conn.getresponse()
-                print "CSBaseRest: status da resposta do csbase: "+ str(response.status)
-                responseData = response.read()
-                print "CSBaseRest: tamanho da resposta: " + str(len(responseData))
-                conn.close()
-                print "CSBaseRest: fechando a conexao"
+                responseData = self.getRestDataFor(configurationAttributes.get("CSBaseRestBaseURL").getValue2(), user_name, user_password)
                 if (responseData.find("accessToken") > 0):
                     logged_in = True
                     responseObj = json.loads(responseData)
                     uid = responseObj["user"]["id"]
                     foundUser = userService.getUserByAttribute("uid", uid)
                     if (foundUser == None):
-                        print "CSBaseRest: criando usuario para: " + uid
-                        fullName = responseObj["user"]["name"]
-                        firstName = fullName.split()[0]
-                        surName = fullName.partition(" ")[-1]
-                        uid = responseObj["user"]["id"]
-                        login = responseObj["user"]["login"]      
-                        newUser = User()
-                        newUser.setAttribute("sn", surName)
-                        newUser.setAttribute("uid", uid)
-                        newUser.setAttribute("cn", fullName)
-                        newUser.setAttribute("displayName", firstName)
-                        newUser.setAttribute("mail", login + "@tecgraf.puc-rio.br")
-                        newUser.setAttribute("profile", "CSBase")
-                        foundUser = userService.addUser(newUser, True)
-                    authServiceRet = authenticationService.authenticate(foundUser.getUserId())
-                    print "CSBaseRest: retorno do authService: "+str(authServiceRet)
-            print "CSBaseRest: terminou"
+                        foundUser = self.createUserFor(responseObj, userService)
+                    authenticationService.authenticate(foundUser.getUserId())
             if (not logged_in):
                 return False
 
@@ -100,22 +69,34 @@ class PersonAuthentication(PersonAuthenticationType):
         else:
             return False
         
-    '''
+    def createUserFor(self, responseObj, userService):
+        print "CSBaseRest: criando usuario para: " + responseObj["user"]["id"]
+        fullName = responseObj["user"]["name"]
+        firstName = fullName.split()[0]
+        surName = fullName.partition(" ")[-1]
+        uid = responseObj["user"]["id"]
+        login = responseObj["user"]["login"]      
+        newUser = User()
+        newUser.setAttribute("sn", surName)
+        newUser.setAttribute("uid", uid)
+        newUser.setAttribute("cn", fullName)
+        newUser.setAttribute("displayName", firstName)
+        newUser.setAttribute("mail", login + "@tecgraf.puc-rio.br")
+        newUser.setAttribute("profile", "CSBase")
+        foundUser = userService.addUser(newUser, True)
+        return foundUser
+    
     def getRestDataFor(self, baseurl, login, password):
+        data = ""
         print "CSBaseRest: acessando csbase na url base: "+baseurl
         params = urllib.urlencode({'login': login, 'password': password})
         headers = {"Content-type": "application/x-www-form-urlencoded"}
-        print "CSBaseRest: conectando..."
         conn = httplib.HTTPConnection(baseurl)
-        print "CSBaseRest: fazendo requisicao..."
         conn.request("POST", "/v1/authentication", params, headers)
-        print "CSBaseRest: obtendo reposta..."
         response = conn.getresponse()
-        print "CSBaseRest: status da resposta do csbase: "+ str(response.status)
         data = response.read()
         conn.close()
         return data
-    '''
     
     def prepareForStep(self, configurationAttributes, requestParameters, step):
         if (step == 1):
